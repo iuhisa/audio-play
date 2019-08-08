0d3d35ac93b5200c3e5cdfdb1fb587267395f2f8
iuhisa <iuhisa1271@gmail.com>
2019-08-08 23:53:21 +0900

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
-module: {
-  rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      module.exports = {
+        use: {
+          loader: 'babel-loader',
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
-    }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
   ]
 }
